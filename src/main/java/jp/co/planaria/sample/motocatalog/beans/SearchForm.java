337628cbfa78526f8bea8408628f51471a6a0e5a
Voice033 <shiori@i-voice.co.jp>
2026-04-28 16:16:37 +0900

    検索条件を入力チェックする

--- src/main/java/jp/co/planaria/sample/motocatalog/beans/SearchForm.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/beans/SearchForm.java
@@ -1,5 +1,6 @@
 package jp.co.planaria.sample.motocatalog.beans;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -11,6 +12,7 @@ public class SearchForm {
     //ブランドID
     private String brandId;
     //キーワード
+    @Size(min = 2, max = 10)
     private String keyword;
     
 }
