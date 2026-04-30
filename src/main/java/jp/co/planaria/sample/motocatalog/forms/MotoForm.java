29fdec768bb602e9361796670f2a08dfd0742684
Voice033 <shiori@i-voice.co.jp>
2026-04-30 14:27:13 +0900

    情報を更新できるようになった

---
+++ src/main/java/jp/co/planaria/sample/motocatalog/forms/MotoForm.java
@@ -0,0 +1,32 @@
+package jp.co.planaria.sample.motocatalog.forms;
+
+
+import jp.co.planaria.sample.motocatalog.beans.Brand;
+import lombok.Data;
+
+/**
+ * 更新画面の入力内容
+ */
+@Data
+public class MotoForm {
+    // バイク番号
+    private Integer motoNo;
+    // バイク名
+    private String motoName;
+    // シート高
+    private Integer seatHeight;
+    // シリンダー
+    private Integer cylinder;
+    // 冷却
+    private String cooling;
+    // 価格
+    private Integer price;
+    // コメント
+    private String comment;
+    // ブランドID
+    private Brand brandId;
+    // バージョン
+    private Integer version;
+
+
+}
