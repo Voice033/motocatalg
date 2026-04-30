af10e03bddd45e29d4ddd292afe8868e698eeecc
Voice033 <shiori@i-voice.co.jp>
2026-04-30 15:39:41 +0900

    エラーメッセージを整理した

--- src/main/java/jp/co/planaria/sample/motocatalog/services/MotosService.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/services/MotosService.java
@@ -1,8 +1,10 @@
 package jp.co.planaria.sample.motocatalog.services;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,18 +19,21 @@ import jp.co.planaria.sample.motocatalog.mappers.MotorcycleMapper;
 public class MotosService {
 
     @Autowired
+    MessageSource messageSource;
+
+    @Autowired
     MotorcycleMapper motorcycleMapper;
 
     @Autowired
     BrandMapper brandMapper;
-    
+
     public List<Motorcycle> getMotos(SearchForm condition) {
-        return motorcycleMapper.selectByCondition(condition); 
+        return motorcycleMapper.selectByCondition(condition);
 
     }
 
     public Motorcycle getMotos(int motoNo) {
-        return motorcycleMapper.selectByPK(motoNo); 
+        return motorcycleMapper.selectByPK(motoNo);
 
     }
 
@@ -36,22 +41,27 @@ public class MotosService {
         return brandMapper.selectAll();
 
     }
-    
+
     /**
      * バイク情報を更新する
+     * 
      * @param moto バイク情報
      * @return 更新件数
      */
     @Transactional
     public int save(Motorcycle moto) {
         int cnt = motorcycleMapper.update(moto);
-        //更新できなかった場合、更新されたか削除されたため楽観的排他エラーとする
-        if(cnt == 0){
-            throw new OptimisticLockingFailureException("楽観的排他エラー");
+        // 更新できなかった場合、更新されたか削除されたため楽観的排他エラーとする
+        if (cnt == 0) {
+            throw new OptimisticLockingFailureException(
+                    messageSource.getMessage("error.OptimisticLockingFailure", 
+                    null, Locale.JAPANESE));
         }
-        //2件以上更新は想定外（SQL不備の可能性）
-        if (cnt > 1){
-            throw new RuntimeException("2件以上更新されました");
+        // 2件以上更新は想定外（SQL不備の可能性）
+        if (cnt > 1) {
+            throw new RuntimeException(
+                    messageSource.getMessage("error.Runtime", 
+                    new String[] { "2件以上更新されました。" }, Locale.JAPANESE));
         }
         return cnt;
     }
